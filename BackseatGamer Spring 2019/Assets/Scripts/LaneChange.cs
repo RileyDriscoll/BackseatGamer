074f8418f4174c741a43527a287ee1442b0a0735
Denbus <32473429+Denbus@users.noreply.github.com>
2019-03-30 18:07:32 -0700

    CarLevelManager just about done
    LaneChanger needs hooked up properly

---
+++ BackseatGamer Spring 2019/Assets/Scripts/LaneChange.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaneChange : MonoBehaviour
+{
+
+    public float duration;
+    private Vector3 startPos;
+    //private GameObject car;
+    private float timeElapsed;
+    private float totalDeltaX;
+    private bool changing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (changing == true)
+        {
+            timeElapsed += Time.deltaTime;
+            if (timeElapsed < duration)
+            {
+                transform.position = new Vector3(startPos.x + (totalDeltaX / timeElapsed), startPos.y, startPos.z);
+            }
+            else
+            {
+                changing = false;
+            }
+        }
+    }
+
+    public void StartChanging()
+    {
+        if (changing == false)
+        {
+            startPos = transform.position;
+            if (startPos.x < 0)
+            {
+                totalDeltaX = 4;
+            }
+            else
+            {
+                totalDeltaX = -4;
+            }
+            changing = true;
+            timeElapsed = .1F;
+        }
+    }
+}
