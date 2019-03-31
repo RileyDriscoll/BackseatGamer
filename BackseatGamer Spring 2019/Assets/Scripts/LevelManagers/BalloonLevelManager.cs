f6d7454ba2b08d66910871d0185c62200fb2db9e
Denbus <32473429+Denbus@users.noreply.github.com>
2019-03-30 21:16:10 -0700

    Ballon Level Shenanigans
    Pump is pumping, balloon animations are kinda fucked right now though

---
+++ BackseatGamer Spring 2019/Assets/Scripts/LevelManagers/BalloonLevelManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonLevelManager : LevelManager
+{
+    public float cycleTime; //2.5
+    public float cycleDistance; //0.0045
+    public GameObject pumpHandle;
+
+    private float timePassed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timePassed = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pumpHandle.transform.position += new Vector3(0, cycleDistance * (Mathf.Sin((Mathf.PI * .5f * timePassed))));
+        timePassed += Time.deltaTime;
+    }
+}
